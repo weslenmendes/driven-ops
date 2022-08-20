9b46a164f35842474648f225e7a7c416e61fd981
Weslen Mendes <weslenmends@gmail.com>
2022-08-20 16:56:09 -0300

    feat: created initial route in frontend

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import AppRoutes from "../routes";
 
 function App() {
-  const [student, setStudent] = useState(null);
-    
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if(!student){
-        alert("Putz! Não há estudantes cadastrados para o sorteio!");
-      } else {
-        setStudent(student);
-      }
-    } catch (error) {
-      alert("Não foi possível realizar o sorteio!");
-    }
-  }, []);
-  
-  return (
-    student ? <h1>{student.name}</h1> : "Carregando..."
-  )
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ front-end/src/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Home(props) {
+  const [student, setStudent] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const API_URL = process.env.REACT_APP_BASE_URL;
+
+      try {
+        const response = await axios.get(`${API_URL}/students/random`);
+        const student = response.data;
+        if (!student) {
+          alert("Putz! Não há estudantes cadastrados para o sorteio!");
+        } else {
+          setStudent(student);
+        }
+      } catch (error) {
+        alert("Não foi possível realizar o sorteio!");
+      }
+    })();
+  }, []);
+
+  return student ? <h1>{student.name}</h1> : "Carregando...";
+}
